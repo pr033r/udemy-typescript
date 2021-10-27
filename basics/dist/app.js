be16203f22431f7d15264b7607c1295960375f0e
Adam Lasak <lasak.ad@gmail.com>
2021-10-27 15:09:23 +0200

    private constructor -> singleton

--- basics/dist/app.js
+++ basics/dist/app.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __rest = (this && this.__rest) || function (s, e) {
     var t = {};
     for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
@@ -29,16 +44,27 @@ var hobby1 = hobbies[0], hobby2 = hobbies[1], remainingHobbies = hobbies.slice(2
 var userName = user.name, restParams = __rest(user, ["name"]);
 console.log(userName, restParams);
 var addShort = function (a, b) { return a + b; };
-var User = (function () {
-    function User(login, password) {
-        this.login = login;
-        this.password = password;
-        this.counterForLogout = 360;
+var Logger = (function () {
+    function Logger() {
     }
+    return Logger;
+}());
+var User = (function (_super) {
+    __extends(User, _super);
+    function User(login, password) {
+        var _this = _super.call(this) || this;
+        _this.login = login;
+        _this.password = password;
+        _this.counterForLogout = 360;
+        return _this;
+    }
+    User.prototype.log = function (message, code) {
+        console.log(code, message);
+    };
     Object.defineProperty(User.prototype, "counter", {
         get: function () {
             if (this.counterForLogout <= 0) {
-                throw new Error('Your session expired');
+                throw new Error("Your session expired");
             }
             return this.counterForLogout;
         },
@@ -48,6 +74,12 @@ var User = (function () {
         enumerable: false,
         configurable: true
     });
+    User.getInstance = function () {
+        if (this.instance) {
+            return this.instance;
+        }
+        return new User('lasakada', '12345');
+    };
     User.prototype.whosLogged = function () {
         console.log("> " + this.login + " logged");
     };
@@ -56,8 +88,8 @@ var User = (function () {
     };
     User.VERSION = "1.1";
     return User;
-}());
-var loginUser = new User("lasakada@tietoevry.com", "12345");
+}(Logger));
+var loginUser = User.getInstance();
 var coppyUser = {
     decreaseCounterOfLogout: loginUser.decreaseCounterOfLogout,
 };
--- basics/src/app.ts
+++ basics/src/app.ts
@@ -19,14 +19,22 @@ console.log(userName, restParams);
 const addShort: (a: number, b: number) => number = (a, b) => a + b;
 
 //-------------------------------------------------------------------------------------
-// for checking the generated US file
-class User {
+// Check the generated US file
+abstract class Logger {
+  abstract log(message: string, code?: string | number): void;
+}
+
+class User extends Logger {
+  log(message: string, code?: string | number): void {
+    console.log(code, message);
+  }
   private counterForLogout = 360;
   public static readonly VERSION = "1.1";
+  private static instance: User;
 
   public get counter(): number {
-    if (this.counterForLogout <= 0){
-      throw new Error('Your session expired');
+    if (this.counterForLogout <= 0) {
+      throw new Error("Your session expired");
     }
     return this.counterForLogout;
   }
@@ -35,7 +43,17 @@ class User {
     this.counterForLogout = v;
   }
 
-  constructor(public login: string, public password: string) {}
+  // with private, we enforces that user cannot create instance -> singleton
+  private constructor(public login: string, public password: string) {
+    super();
+  }
+
+  static getInstance() {
+    if (this.instance) {
+      return this.instance;
+    }
+    return new User('lasakada', '12345');
+  }
 
   whosLogged() {
     console.log(`> ${this.login} logged`);
@@ -47,7 +65,8 @@ class User {
   }
 }
 
-const loginUser = new User("lasakada@tietoevry.com", "12345");
+// const loginUser = new User("lasakada@tietoevry.com", "12345"); // not working with singleton
+const loginUser = User.getInstance();
 const coppyUser = {
   decreaseCounterOfLogout: loginUser.decreaseCounterOfLogout,
 };
