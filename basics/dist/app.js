8a8c0519a15c9ba5013439ced2d1e6bb1d67536d
Adam Lasak <lasak.ad@gmail.com>
2021-10-29 16:26:08 +0200

    different types of decorators

--- basics/dist/app.js
+++ basics/dist/app.js
@@ -5,14 +5,20 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function DLogger(log) {
+    console.log('DLOGGER FACTORY');
     return function (constructor) {
         console.log(log);
         console.log(constructor);
     };
 }
 function ViewTemplate(data) {
+    console.log('ViewTemplate FACTORY');
     return function (constructor) {
+        console.log('Rendering template');
         if (data.selector && data.template) {
             var el = document.querySelector(data.selector);
             var p = new constructor();
@@ -25,13 +31,63 @@ function ViewTemplate(data) {
         }
     };
 }
+function PropertyDecorator(target, name) {
+    var color = 'color: #a00;';
+    console.log('%c @PropertyDecorator', color);
+    console.log("%c \t name = " + name, color);
+    console.log('%c \t target = ' + target, color);
+}
+function AccessorDecorator(target, name, descriptor) {
+    var color = 'color: #0a0;';
+    console.log('%c @AccessorDecorator', color);
+    console.log("%c \t name = " + name, color);
+    console.log("%c \t target = " + target, color);
+    console.log("%c \t descriptor = " + descriptor, color);
+}
+function MethodDecorator(target, name, descriptor) {
+    var color = 'color: #00a;';
+    console.log('%c @MethodDecorator', color);
+    console.log("%c \t name = " + name, color);
+    console.log("%c \t target = " + target, color);
+    console.log("%c \t descriptor = " + descriptor, color);
+}
+function ParamDecorator(target, nameOfMethod, position) {
+    var color = 'color: #abc;';
+    console.log('%c @ParamDecorator', color);
+    console.log("%c \t nameOfMethod = " + nameOfMethod, color);
+    console.log("%c \t target = " + target, color);
+    console.log("%c \t position = " + position, color);
+}
 var DPerson = (function () {
-    function DPerson() {
+    function DPerson(paramDec) {
+        if (paramDec === void 0) { paramDec = 'paramDecoratorProperty'; }
         this.name = 'Using custom decorators';
         this.subtitle = '@ViewTemplate';
         this.text = "The syntax for using this decorator is: <br>\n    <span style=\"color: #00a;\">@ViewTemplate(data: {selector: string, template: string})</span>";
+        this.paramDecoratorProperty = paramDec;
         console.log('Calling a Person constructor...');
     }
+    Object.defineProperty(DPerson.prototype, "paramDecorator", {
+        set: function (v) {
+            this.paramDecoratorProperty = v;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    DPerson.prototype.getString = function (a, b) {
+        return a + b;
+    };
+    __decorate([
+        PropertyDecorator
+    ], DPerson.prototype, "paramDecoratorProperty", void 0);
+    __decorate([
+        AccessorDecorator
+    ], DPerson.prototype, "paramDecorator", null);
+    __decorate([
+        MethodDecorator,
+        __param(0, ParamDecorator),
+        __param(1, ParamDecorator)
+    ], DPerson.prototype, "getString", null);
     DPerson = __decorate([
         DLogger('DECORATOR-PARAM'),
         ViewTemplate({
--- basics/src/app.ts
+++ basics/src/app.ts
@@ -1,14 +1,19 @@
 function DLogger(log: string) {
   // using an Decorator Factory - usefull when we want to pass arguments
   // into the decorator
-  return function(constructor: Function) {
+  console.log('DLOGGER FACTORY');
+
+  return function (constructor: Function) {
     console.log(log);
     console.log(constructor);
-  }
+  };
 }
 
-function ViewTemplate(data: {selector: string, template: string}) {
-  return function(constructor: any) {
+function ViewTemplate(data: { selector: string; template: string }) {
+  console.log('ViewTemplate FACTORY');
+
+  return function (constructor: any) {
+    console.log('Rendering template');
     if (data.selector && data.template) {
       const el = document.querySelector(data.selector);
       const p = new constructor();
@@ -19,7 +24,46 @@ function ViewTemplate(data: {selector: string, template: string}) {
         el.querySelector('p')!.innerHTML = p.text;
       }
     }
-  }
+  };
+}
+
+function PropertyDecorator(target: any, name: string | Symbol) {
+  const color = 'color: #a00;';
+  console.log('%c @PropertyDecorator', color);
+  console.log(`%c \t name = ${name}`, color);
+  console.log('%c \t target = ' + target, color);
+}
+
+function AccessorDecorator(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  const color = 'color: #0a0;';
+  console.log('%c @AccessorDecorator', color);
+  console.log(`%c \t name = ${name}`, color);
+  console.log(`%c \t target = ${target}`, color);
+  console.log(`%c \t descriptor = ${descriptor}`, color);
+}
+
+function MethodDecorator(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  const color = 'color: #00a;';
+  console.log('%c @MethodDecorator', color);
+  console.log(`%c \t name = ${name}`, color);
+  console.log(`%c \t target = ${target}`, color);
+  console.log(`%c \t descriptor = ${descriptor}`, color);
+}
+
+function ParamDecorator(target:any, nameOfMethod: string, position: number) {
+  const color = 'color: #abc;';
+  console.log('%c @ParamDecorator', color);
+  console.log(`%c \t nameOfMethod = ${nameOfMethod}`, color);
+  console.log(`%c \t target = ${target}`, color);
+  console.log(`%c \t position = ${position}`, color);
 }
 
 @DLogger('DECORATOR-PARAM')
@@ -33,8 +77,22 @@ class DPerson {
   text = `The syntax for using this decorator is: <br>
     <span style="color: #00a;">@ViewTemplate(data: {selector: string, template: string})</span>`;
 
-  constructor() {
+  @PropertyDecorator
+  private paramDecoratorProperty: string;
+
+  @AccessorDecorator
+  public set paramDecorator(v: string) {
+    this.paramDecoratorProperty = v;
+  }
+
+  constructor(paramDec: string = 'paramDecoratorProperty') {
+    this.paramDecoratorProperty = paramDec;
     console.log('Calling a Person constructor...');
+  }
+
+  @MethodDecorator
+  getString(@ParamDecorator a: number, @ParamDecorator b: number) {
+    return a + b;
   }
 }
 
